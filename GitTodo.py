4c37f46ba5f0be205b42be5a771b534790deab33
Jonathan Hipkiss <hipkiss91@users.noreply.github.com>
2016-09-26 10:16:21 +0100

    Update GitTodo.py
    See messages 0.2.0.txt

--- GitTodo.py
+++ GitTodo.py
@@ -7,11 +7,14 @@ import requests
 import json
 import subprocess
 import traceback
+import fnmatch
+
+todos_array = ['Todo', 'TODO', 'todo']
 
 def get_setting(key, default=None):
-    settings = sublime.load_settings('GitTodo.sublime-settings')
-    os_specific_settings = {}
-    return os_specific_settings.get(key, settings.get(key, default))
+	settings = sublime.load_settings('GitTodo.sublime-settings')
+	os_specific_settings = {}
+	return os_specific_settings.get(key, settings.get(key, default))
 
 def getoutput(command):
 	out, err = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()
@@ -21,7 +24,7 @@ def get_repo_details(self):
 	# only works on current open file
 	path, filename = os.path.split(self.view.file_name())
 
-	# Switch to cmd of file
+	# Switch to cwd of file
 	os.chdir(path + "/")
 	git_config_path = getoutput("git remote show origin -n")
 	p = re.compile(r"(.+: )*([\w\d\.]+)[:|@]/?/?(.*)")
@@ -32,11 +35,11 @@ def get_repo_details(self):
 		git_config = parts[0][2]
 
 	if ':' in git_config:
-		# if SSH: {domain}:{user}/{repo}.git
+		# SSH repository format: {domain}:{user}/{repo}.git
 		domain, user, repo = git_config.replace(".git", "").replace(":", "/").split("/")
 		return ('ssh', domain, user, repo)
 	else:
-		# if HTTP: {domain}/{user}/{repo}.git
+		# HTTP repository format: {domain}/{user}/{repo}.git
 		domain, user, repo = git_config.replace(".git", "").split("/")
 		return ('https', domain, user, repo)
 
@@ -49,20 +52,7 @@ class GitTodoFindCommand(sublime_plugin.TextCommand):
 		config = get_repo_details(self)
 		if config[0] == False:
 			return(print(config[1]))
-		if len(projects) < 1:
-			if 'bitbucket' in config[1]:
-				print('bitbucket Not currently supported')
-				# return(print('bitbucket Not currently supported'))
-			if '192.168' in config[1]:
-				print('private local repos Not currently supported')
-				# return(print('private local repos Not currently supported'))
-			GitTodoFindCommand.process_project(directories, config[3])
-		else:
-			for project in projects:
-				if config == project:
-					GitTodoFindCommand.process_project(directories, project)
-				else:
-					GitTodoFindCommand.process_project(directories, config[3])
+		GitTodoFindCommand.process_project(directories, config[3])
 
 	def process_project(project_dir, project):
 		dir_path = os.path.dirname(project_dir + project + '\\')
@@ -77,96 +67,110 @@ class GitTodoFindCommand(sublime_plugin.TextCommand):
 
 			file = open(nthfile) # Open the nth file.
 			for num, line in enumerate(file, 1): # Read each line in the file.
-				if 'TODO' in line or 'todo' in line or 'Todo' in line: # Check each line for a 'todo'.
-					GitTodoFindCommand.process_line(filename, num, line, todo_array)
+				for todo in todos_array:
+					if todo in line: # Check each line for a 'todo'.
+						GitTodoFindCommand.process_line(filename, num, line, todo_array)
 			if len(todo_array) > 0:
 				all_todos.append(todo_array)# append todos for each file into array of files.
 		GitTodoFindCommand.store_local_todos(project, all_todos, dir_path)
-		# GitTodoFindCommand.process_todo_array(all_todos)
 
 	def get_filepaths(directory):
 		""" This function will generate the file names in a directory tree by walking the tree either top-down or bottom-up. For each
 		directory in the tree rooted at directory top (including top itself), it yields a 3-tuple (dirpath, dirnames, filenames).
 		"""
 		file_paths = []  # List which will store all of the full filepaths.
-
+		excluded_folders = get_setting('exclude_folders', [])
+		excluded_files = get_setting('exclude_files', [])
 		# Walk the tree.
 		for root, directories, files in os.walk(directory):
-			# Currently this only does the current directory, need to adapt for all
-			for filename in files:
-				# Determine file type.
-				ext = filename.split('.')[-1]
-				if ext == 'js' or ext == 'html' or ext == 'css':
-					# Join the two strings in order to form the full filepath.
-					filepath = os.path.join(root, filename)
+			for directory in directories:
+				if directory in excluded_folders:
+					directories.remove(directory)
+			for file in files:
+				ext = GitTodoFindCommand.MatchesExtensions(file, excluded_files)
+				if ext == False:
+					filepath = os.path.join(root, file)
 					file_paths.append(filepath)  # Add it to the list.
+		return file_paths
 
-		return file_paths  # Self-explanatory.
+	def MatchesExtensions(name,extensions):
+		for extension in extensions:
+			if fnmatch.fnmatch(name, extension):
+				return True
+		return False
 
 	def process_line(file, num, line, todo_array):
+		mytodo = Gittodo()
+		mytodo.set_filename(file)
+		mytodo.set_line(num)
 		# This processes the line, it will return the correct object of format for each todo.
-		# todo: [TITLE] Description of task goes here. @someone #bug #minor
-		todo_file = file
-		if line.find('[') < 0 or line.find(']') < 0 or line.find('@') < 0 or line.find('#') < 0:
-			# print('TODO was not correctly formatted in file' + todo_file +', @ line '+str(num)+'. Please follow the formatting guidleines @ [insert URL here].')
-			if line.find('TODO'):
-				file_start = line.find('TODO')
-			if line.find('todo'):
-				file_start = line.find('todo')
-			if line.find('Todo'):
-				file_start = line.find('Todo')
-				pass
-			file_newline = line.find('\n')
+		# todo: [TITLE] (Description of task goes here.) @someone #bug #minor *milestone
+		if not line.find('[') > 0 and not line.find(']', line.find('[')) > 0:
+			print('Todo was not correctly formatted in file' + file +', @ line '+str(num)+'. Please follow the formatting guidleines @ https://github.com/hipkiss91/GitTodo/blob/master/README.md.')
+			print('The minimum required to submit an issue is a title. All incorrectly formatted todos can be found in the gitTodo.txt file')
+			for todo_case in todos_array:
+				if line.find(todo_case) > 0:
+					file_start = line.find(todo_case)
+			file_newline = line.find('\r\n')
 			if file_newline:
 				file_end = len(line) - 2
 			else:
 				file_end = len(line)
-			todo_file = line[file_start:file_end]
-			# todo_array.append(todo_file)
+			todo_bad = line[file_start:file_end]
+			mytodo.set_review(True)
+			mytodo.set_body(todo_bad)
 		else:
 			# Process the title. START
-			if line.find('[') and line.find(']'):
+			if line.find('[') > 0 and line.find(']') > 0:
 				title_start = line.find('[') + 1
 				title_end = line.find(']')
 				todo_title = line[title_start:title_end]
+				mytodo.set_title(todo_title)
 			# Process the title. End
 
 			# Process the summary. START
-			if line.find(']') and line.find('.', line.find(']')):
-				summary_start = line.find(']') + 2
-				summary_end = line.find('.')
-				todo_summary = line[summary_start:summary_end]
+			if line.find('(') > 0 and line.find(')') > 0:
+				body_start = line.find('(') + 1
+				body_end = line.find(')')
+				todo_body = line[body_start:body_end]
+				mytodo.set_body(todo_body)
 			# Process the summary. End
 
 			# Process the owner. START
-			if line.find('@') and line.find(' ', line.find('@')):
-				owner_start = line.find('@') + 1
-				owner_end = line.find(' ', owner_start)
-				todo_owner = line[owner_start:owner_end]
+			if line.find('@') > 0 and line.find(' ', line.find('@')) > 0:
+				assignee_start = line.find('@') + 1
+				assignee_end = line.find(' ', assignee_start)
+				todo_assignee = line[assignee_start:assignee_end]
+				mytodo.set_assignee(todo_assignee)
 			# Process the owner. End
 
 			# Process the labels. START
-			if line.find('#'):
+			if line.find('#') > 0:
 				todo_labels = []
-				num = line.count('#')
+				labels = line.count('#')
 				labels_start = line.find('#')
 				labels_end = len(line.rstrip('\r\n'))
-				if num > 1:
+				if labels > 1:
 					label_string = line[labels_start:labels_end].split(' ')
 					for label in label_string:
 						start = 0
 						end = len(label)
 						todo_labels.append(label[start + 1:end])
+						mytodo.set_labels(todo_labels)
 				else:
 					todo_labels.append(line[labels_start + 1:labels_end])
+					mytodo.set_labels(todo_labels)
 			# Process the labels. End
 
+			# Process the milestones. START
+			if line.find('*') > 0:
+				ms_start = line.find('*') + 1
+				ms_end = line.find(' ', ms_start)
+				todo_ms = line[ms_start:ms_end]
+			# Process the milestones. End
+
 			# Add them all together and pass back to the parent.
-			todo_array.append({	'filename'	:	todo_file,
-								'title'		: 	todo_title,
-								'assignee'	:	todo_owner,
-								'body'		: 	todo_summary,
-								'labels'	:	todo_labels})
+		todo_array.append(mytodo)
 
 	def store_local_todos(repo, todo_array, project_dir):
 		# Store todo's locally in the root project dir.
@@ -175,14 +179,18 @@ class GitTodoFindCommand(sublime_plugin.TextCommand):
 			mode = 'a' if os.path.exists(write_path) else 'w'
 			with open(write_path, mode) as file:
 				path = open(write_path).read()
-				for todo in todo_array:
-					if mode == 'a':
-						if json.dumps(todo) not in path:
-							GitTodoFindCommand.process_todo(repo, todo)
-							file.write(json.dumps(todo) + '  \n')
-					else:
-						GitTodoFindCommand.process_todo(repo, todo)
-						file.write(json.dumps(todo) + '  \n')
+				for todos in todo_array:
+					for todo in todos:
+						issue = json.dumps(todo.__dict__) # JSON dump the object, for file write.
+						if mode == 'a':
+							if issue not in path: # Checks if it's already in the file.
+								if todo.review == False:
+									GitTodoFindCommand.process_todo(repo, todo)
+								file.write(issue + '\n')
+						else:
+							if todo.review == False:
+								GitTodoFindCommand.process_todo(repo, todo)
+							file.write(issue + '\n')
 		except Exception:
 			print(traceback.format_exc())
 
@@ -209,8 +217,46 @@ class GitTodoFindCommand(sublime_plugin.TextCommand):
 		return session
 
 	def create_issue(username, repo, session, todo):
-		# issue = {'filename'	:	todo_file, 'title': 'title', 'assignee':'hipkiss91', 'body':'body', 'labels':['labels']}
 		url = 'https://api.github.com/repos/' + username + '/' + repo + '/issues'
-		issue = {'title': todo[0]['title'], 'assignee': todo[0]['assignee'], 'body': todo[0]['filename'] + '.\n' + todo[0]['body'], 'labels': todo[0]['labels']}
+		issue = {'title': todo.title,
+				 'assignee': todo.assignee,
+				 'body': todo.line + '.   ' + todo.filename + '.\n' + todo.body,
+				 'labels': todo.labels}
 		req = session.post(url, json.dumps(issue))
 		return req.status_code
+
+# A Class for each Todo.
+class Gittodo(object):
+	# Options and methods are self explanatory.
+	review = False # Whether or not to be submitted as an issue or just submitted to the gitTodo.txt for review.
+	filename = None
+	line = None
+	title = None
+	assignee = None
+	body = None
+	labels = None
+	milestone = None
+
+	def set_review(self, review):
+		self.review = review
+
+	def set_filename(self, filename):
+		self.filename = filename
+
+	def set_line(self, line):
+		self.line = line
+
+	def set_title(self, title):
+		self.title = title
+
+	def set_body(self, body):
+		self.body = body
+
+	def set_assignee(self, assignee):
+		self.assignee = assignee
+
+	def set_labels(self, labels):
+		self.labels = labels
+
+	def set_milestone(self, milestone):
+		self.milestone = milestone
